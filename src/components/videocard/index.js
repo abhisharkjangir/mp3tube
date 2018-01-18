8a1db80207d4f517e0626970a8a652b07fb2261d
Abhsihar Jangir <abhishar.jangir@abspl.in>
2018-01-18 20:46:30 +0530

    API CHANGE

--- src/components/videocard/index.js
+++ src/components/videocard/index.js
@@ -26,12 +26,12 @@ class Videocard extends Component {
   fetchMp4Link () {
     this.setState({isLoading : true})
     let fuck = this;
-    fetch(`http://imabhi.herokuapp.com/yt/kvid?id=${this.props.video.id}`)
+    fetch(`http://imabhi.herokuapp.com/yt/kvidsite?id=${this.props.video.id}`)
     .then(r => r.json())
     .then(r => {
-      console.log(r.data.mp4);
-      location.href  = r.data.mp4;
-      fuck.setState({isLoading : false, links: r.data});
+      // console.log(r.data.mp4);
+      // location.href  = r.data.mp4;
+      fuck.setState({isLoading : false, link: r.data.mp4});
     }).catch(err => {
       fuck.setState({isLoading : true});
     })
@@ -40,10 +40,10 @@ class Videocard extends Component {
   fetchMp3Link () {
     this.setState({isLoading : true})
     let fuck = this;
-    fetch(`http://imabhi.herokuapp.com/yt/kvid?id=${this.props.video.id}`)
+    fetch(`http://imabhi.herokuapp.com/yt/kvidsite?id=${this.props.video.id}`)
     .then(r => r.json())
     .then(r => {
-      location.href  = r.data.mp3;
+      // location.href  = r.data.mp3;
       fuck.setState({isLoading : false, links: r.data});
     }).catch(err => {
       fuck.setState({isLoading : true});
@@ -69,21 +69,20 @@ class Videocard extends Component {
         </div>
         <div className="video-actions">
           <div className="row">
-            <div className="col-xs-4">
+            <div className="col-xs-6">
               <p onClick={this.playVideo}>
                 <i className="fa fa-play"></i> Play
               </p>
             </div>
-            {this.state.links  && <code>{this.state.links.mp3}</code>}
-            <div className="col-xs-4">
+            {/* <div className="col-xs-4">
               {!this.state.links && <p onClick={this.fetchMp3Link}>
                 <i className="fa fa-download"></i> Mp3</p>}
-                {this.state.links && <a href={this.state.links.mp3} download> <i className="fa fa-check"></i> Mp3</a>}
-            </div>
-            <div className="col-xs-4" >
-              {!this.state.links && <p onClick={this.fetchMp4Link}>
+                {this.state.links && <a className="download-ready" href={this.state.links.mp3} download> <i className="fa fa-check"></i> Mp3</a>}
+            </div> */}
+            <div className="col-xs-6" >
+              {!this.state.link && <p onClick={this.fetchMp4Link}>
                 <i className="fa fa-download"></i> Mp4</p>}
-                {this.state.links && <a href={this.state.links.mp4} download> <i className="fa fa-check"></i> Mp4</a>}
+                {this.state.link && <a className="download-ready" href={this.state.link} download> <i className="fa fa-check"></i> Mp4</a>}
             </div>
           </div>
         </div>
@@ -96,7 +95,6 @@ class Videocard extends Component {
     )
   }
 }
-
 
 
 export default Videocard
